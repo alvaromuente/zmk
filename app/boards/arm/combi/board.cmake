9d2b6f98f0ddbced8288b5af1733351e60a1bec3
armastardo <brandon.abr@hotmail.com>
2022-06-15 21:52:14 -0500

    Revivir combi

--- app/boards/arm/combi/board.cmake
+++ app/boards/arm/combi/board.cmake
@@ -1,7 +1,6 @@
 # Copyright (c) 2022 Brandon Beltran
 # SPDX-License-Identifier: MIT
 
-set(OPENOCD_NRF5_SUBFAMILY nrf52)
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
